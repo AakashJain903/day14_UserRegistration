1f48dad1bcf8785738669a3367f3f1e9ca5d1b6a
AakashJain903 <aakash.jain903@gmail.com>
2020-07-24 17:03:46 +0530

    Added : LastName Validation UserRegistration

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,9 +1,29 @@
-#!/bin/bash -x
-read -p "Enter the user first name : " firstName
-pattern="^[A-Z]{1}[a-z]{2}"
-if [[ $firstName =~ $pattern ]]
-then
-        echo "Valid First Name"
-else
-        echo "Invalid First Name"
-fi
+#!/bin/bash
+
+function validation(){ 
+	name=$1
+	pattern=$2
+	flag=0
+
+	while [ $flag -eq 0 ]
+	do
+		if [[ $name =~ $pattern ]]
+		then
+			echo "It is Valid"
+			flag=1
+		else
+			echo "It is not valid"
+			read name
+		fi
+	done
+	echo $name
+}
+
+echo "Enter FirstName start with capital letter and has minimum 3 characters"
+read fisrtName
+pattern="^[A-Z]{1}[a-z]{2}" 
+validation $firstName $pattern
+
+echo "Enter LastName start with capital letter and has minimun 3 characters"
+read lastname
+validation $lastName $pattern
